57ea225742cc872923d6bfbf97d9d48e740ee9cd
Aaron <ly1992@qq.com>
2016-03-30 16:23:06 +0800

    'save'

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var gulp = require('gulp');
+var wiredep = require('wiredep').stream;
+var $ = require('gulp-load-plugins')();
+
+gulp.task('connect', function () {
+    var connect = require('connect');
+    var app = connect()
+        .use(require('connect-livereload')({ port: 35729 }))
+        .use(connect.static('app'))
+        .use(connect.static('bower_components'))
+        .use(connect.directory('app'));
+
+    require('http').createServer(app)
+        .listen(9000)
+        .on('listening', function () {
+            console.log('Started connect web server on http://localhost:9000');
+        });
+});
+
+gulp.task('serve', ['connect'], function () {
+    require('opn')('http://localhost:9000');
+});
+
+gulp.task('bowerInstall',function(){
+    gulp.src('./app/index.html')
+        .pipe(wiredep({
+            optional: 'configuration',
+            goes: 'here'
+        }))
+        .pipe(gulp.dest('./app/'));
+});
+
+gulp.task('watch', ['connect', 'serve','bowerInstall'], function () {
+    var server = $.livereload();
+
+    gulp.watch([
+        'app/*.html',
+        'app/styles/**/*.css',
+        'app/scripts/**/*.js'
+    ]).on('change', function (file) {
+        server.changed(file.path);
+    });
+});
+
