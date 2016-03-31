46ae4531547345a059beebed6c99cfca3ccd985d
Aaron <ly1992@qq.com>
2016-03-31 11:38:38 +0800

    'save

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by admin-b on 2016/3/31.
+ * fas
+ */
+define(function (require, exports, module) {
+    //var t = require('./DrawLottery');
+    var $ = require('jquery');
+    console.log($);
+});
