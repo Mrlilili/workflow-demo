45a7cd50d10a287c0dfb31be8e4371b0fb8bfa87
Aaron <ly1992@qq.com>
2016-03-31 11:39:10 +0800

    'save'

---
+++ app/scripts/DrawLottery.js
@@ -0,0 +1,14 @@
+define(function (require, exports, module) {
+    var $ = require('jquery');
+    function DrawLottery(lotteryContainer, startBtn) {
+        console.log($);
+        //var $LC = $('.' + lotteryContainer);
+        //var $SB = $('.' + startBtn);
+    };
+    DrawLottery.prototype = {
+        init: function () {
+
+        }
+    };
+    module.exports = DrawLottery;
+});
